de32f5a588c9d9aa4321c0d0e4720b89b5c363cd
Flandre3569 <1332726596@qq.com>
2022-02-10 15:52:07 +0800

    koa-upload

---
+++ 回顾koa/koa-file-upload.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const Koa = require('koa')
+const Router = require('koa-router')
+const multer = require('@koa/multer')
+
+const app = new Koa()
+const uploadRouter = new Router({prefix: '/upload'})
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({
+  storage
+})
+
+uploadRouter.post('/avatar', upload.single('file'), (ctx, next) => {
+  console.log(ctx.request.file);
+  ctx.response.body = '上传成功';
+})
+
+app.use(uploadRouter.routes())
+app.use(uploadRouter.allowedMethods())
+
+app.listen(3000);
