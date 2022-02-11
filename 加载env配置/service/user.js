6e20db4b612c7db1f2e15eb604e48938aeb7f52a
Flandre3569 <1332726596@qq.com>
2022-02-11 19:54:37 +0800

    'debug'

--- 加载env配置/service/user.js
+++ 加载env配置/service/user.js
@@ -3,7 +3,7 @@ const conn = require('../app/database')
 class UserService {
   async register(user) {
     const { username, password } = user;
-    const sql = ('INSERT INTO `users`(`name`, `password`) VALUES (?, ?);');
+    const sql = 'INSERT INTO `users`(`name`, `password`) VALUES (?, ?);';
     const result = await conn.execute(sql, [username, password]);
     return result;
   }
