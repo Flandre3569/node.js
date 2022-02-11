47c65b972ac5d29af525437da28cab65a0a7e43c
Flandre3569 <1332726596@qq.com>
2022-02-11 20:48:30 +0800

    judge

--- 加载env配置/middleware/verify.js
+++ 加载env配置/middleware/verify.js
@@ -3,7 +3,7 @@ const errorTypes = require('../errors/error-types');
 const userVerify = async (ctx, next) => {
   const { username, password } = ctx.request.body;
 
-  if (!username || !password || username === '' || password === '') {
+  if (!username || !password) {
     const error = new Error(errorTypes.NO_USER_OR_PASSWORD)
     return ctx.app.emit('error', error, ctx)
   }
